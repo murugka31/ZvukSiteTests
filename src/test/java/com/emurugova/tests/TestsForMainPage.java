19571f122bd84080b45d38bc5ac33566a010a56e
Елена <murugka31@gmail.com>
2023-01-29 13:53:16 +0300

    change code in tests

--- src/test/java/com/emurugova/tests/TestsForMainPage.java
+++ src/test/java/com/emurugova/tests/TestsForMainPage.java
@@ -32,7 +32,7 @@ public class TestsForMainPage extends TestBase {
     void mainPageOpeningTest (String menuItem){
     step("Open main page of Zvuk site", () -> {
         mainPage.openPage();
-        sleep(7000);
+        sleep(8000);
     });
 
     step("Check the main page", () -> {
